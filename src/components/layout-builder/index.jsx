07605733d2f71ed6500235c15d7ce61d1acb4551
iamraihan <trueraihan@gmail.com>
2024-05-22 14:29:17 +0600

    fix: color issues fix

--- src/components/layout-builder/index.jsx
+++ src/components/layout-builder/index.jsx
@@ -43,7 +43,7 @@ const LayoutBuilder = () => {
   };
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-gray-200">
       {partitions.map((partition) => (
         <Partition
           key={partition.id}
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "react-resizable/css/styles.css";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes}>
-      <App />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
